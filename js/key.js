2d8581154690650d1f707221ff79750e0add5fa5
rockmanx41024 <rockmanx41024@yahoo.com.tw>
2015-06-10 20:00:38 +0800

    aaa

--- js/key.js
+++ js/key.js
@@ -1,7 +1,6 @@
 /*請貼上fackbook初始化的相關程式碼*/
 /*https://developers.facebook.com/*/
 
-<script>
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '1608556059426766',
@@ -17,7 +16,7 @@
      js.src = "//connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
-</script>
+
 
 
 
